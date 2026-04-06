b88b5fc005cd8f0fb072a10174c22a04bc4e93bf
Ian Piccinin <ian.piccinin@gmail.com>
2026-04-06 13:51:31 -0300

    Adiciona primeira logística de entrada e validação de letra

--- forca.py
+++ forca.py
@@ -1,1 +1,18 @@
-print('Jogo da Forca')
+import random
+
+print('===Jogo da Forca===')
+
+# Lista de palavras
+palavras = ['teste', 'teste2']
+
+palavra_secreta = random.choice(palavras)
+
+letra = str(input("Digite uma letra: "))
+
+print(palavra_secreta)
+
+if letra in palavra_secreta:
+  print("Acertou uma letra")
+  
+else:
+  print("Errou!")
