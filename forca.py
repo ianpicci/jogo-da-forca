5446df939b32dc4fb365f8860b5760a794d5b9f4
Ian Piccinin <ian.piccinin@gmail.com>
2026-04-06 17:53:53 -0300

    Adiciona loop principal e revela letras acertadas

--- forca.py
+++ forca.py
@@ -1,28 +1,19 @@
 import random
 
-print('===Jogo da Forca===')
+print("===Jogo da Forca===")
 
-# Lista de palavras
-palavras = ['teste', 'teste2']
+palavra_secreta = "teste"
+letras_descobertas = ["_", "_", "_", "_", "_"]
 
-palavra_secreta = random.choice(palavras)
+while True:
+    print("\nPalavra:", " ".join(letras_descobertas))
+    letra = input("Digite uma letra: ")
 
-letra = str(input("Digite uma letra: "))
+    if letra in palavra_secreta:
+        for i in range(len(palavra_secreta)):
+            if palavra_secreta[i] == letra:
+                letras_descobertas[i] = letra
+        print("Acertou!")
+    else:
+        print("Errou!")
 
-print(palavra_secreta)
-
-if letra in palavra_secreta:
-  print("Acertou uma letra")
-  
-else:
-  print("Errou!")
-
-palavra_mostrada = ""
-
-for caractere in palavra_secreta:
-  if caractere == letra:
-    palavra_mostrada += caractere
-  else:
-    palavra_mostrada += "_"
-
-print("Palavra:", palavra_mostrada)
