b2dcdece5a594603b905c41b6360d7d201018469
Ian Piccinin <ian.piccinin@gmail.com>
2026-04-06 18:13:50 -0300

    Informa letras já tentadas e impede repetição

--- forca.py
+++ forca.py
@@ -5,16 +5,24 @@ print("=== Jogo da Forca ===")
 palavra_secreta = "teste"
 letras_descobertas = ["_", "_", "_", "_", "_"]
 tentativas = 6
+letras_tentadas = []
 
 while tentativas > 0:
     print("\nPalavra:", " ".join(letras_descobertas))
     print("Tentativas restantes:", tentativas)
+    print("Letras tentadas:", ", ".join(letras_tentadas))
 
     letra = input("Digite uma letra: ").lower()
 
     if len(letra) != 1 or not letra.isalpha():
         print("Digite apenas uma letra válida.")
         continue
+
+    if letra in letras_tentadas:
+        print("Você já tentou essa letra.")
+        continue
+
+    letras_tentadas.append(letra)
 
     if letra in palavra_secreta:
         for i in range(len(palavra_secreta)):
